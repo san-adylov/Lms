f3891b943ce7849e7bf80f1e15c1174ee7ee5027
NITRO <sanadylov@gmail.com>
2023-09-18 23:24:16 +0600

    add method userDetailsService

--- src/main/java/com/example/lms/configs/security/ApplicationConfig.java
+++ src/main/java/com/example/lms/configs/security/ApplicationConfig.java
@@ -1,15 +1,27 @@
 package com.example.lms.configs.security;
 
 
+import com.example.lms.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @EnableWebSecurity
 @Configuration
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class ApplicationConfig {
+
+  private final UserRepository userRepository;
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return username -> userRepository.getUserByEmail(username).orElseThrow(
+        () -> new NotFoundException("User with email: %s not found".formatted(username)));
+  }
+
 
 }
