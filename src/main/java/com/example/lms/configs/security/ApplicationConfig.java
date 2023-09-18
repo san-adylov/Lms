655ac236d668a3996ca94c0309f03d7d45fc361c
NITRO <sanadylov@gmail.com>
2023-09-18 23:24:45 +0600

    add method passwordEncoder

--- src/main/java/com/example/lms/configs/security/ApplicationConfig.java
+++ src/main/java/com/example/lms/configs/security/ApplicationConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -23,5 +25,9 @@ public class ApplicationConfig {
         () -> new NotFoundException("User with email: %s not found".formatted(username)));
   }
 
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
 }
