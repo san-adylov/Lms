77998c0fcae9872fa56a6bfe002762d44889cbbd
NITRO <sanadylov@gmail.com>
2023-09-19 20:36:50 +0600

    Add NotFoundException class

--- src/main/java/com/example/lms/configs/security/JWTFilter.java
+++ src/main/java/com/example/lms/configs/security/JWTFilter.java
@@ -1,10 +1,55 @@
 package com.example.lms.configs.security;
+
+import com.example.lms.entities.User;
+import com.example.lms.repository.UserRepository;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 
 @Component
 @RequiredArgsConstructor
-public class JWTFilter {
+public class JWTFilter extends OncePerRequestFilter {
+
+  private final JWTService jwtService;
+  private final UserRepository userRepository;
+
+  @Override
+  protected void doFilterInternal(
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain) throws ServletException, IOException {
+    final String tokenHeader = request.getHeader("Authorization");
+    if (tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+      String token = tokenHeader.substring(7);
+      if (StringUtils.hasText(token)) {
+        try {
+          String username;
+          try {
+            username = jwtService.validateToken(token);
+          } catch (MalformedJwtException e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+            return;
+          } catch (ExpiredJwtException e) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+            return;
+          }
+          String finalUsername = username;
+          User user = userRepository.getUserByEmail(username)
+              .orElseThrow(() -> new );
+
+        }
+      }
 
 
-}
+    }
+  }
