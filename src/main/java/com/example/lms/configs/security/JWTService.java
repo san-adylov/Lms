4c121a33050374d98c50c9bdd7c212836444b4d9
NITRO <sanadylov@gmail.com>
2023-09-19 20:30:08 +0600

    Add validate token

--- src/main/java/com/example/lms/configs/security/JWTService.java
+++ src/main/java/com/example/lms/configs/security/JWTService.java
@@ -1,7 +1,9 @@
 package com.example.lms.configs.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import java.time.ZonedDateTime;
 import java.util.Date;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +24,14 @@ public class JWTService {
         .withIssuedAt(new Date())
         .withExpiresAt(Date.from(ZonedDateTime.now().plusWeeks(2).toInstant()))
         .sign(Algorithm.HMAC512(SECRET_KEY));
+  }
+
+  public String validateToken(String token) {
+    JWTVerifier jwtVerifier =
+        JWT.require(Algorithm.HMAC512(SECRET_KEY))
+            .build();
+    DecodedJWT jwt = jwtVerifier.verify(token);
+    return jwt.getClaim("username").asString();
   }
 
 
