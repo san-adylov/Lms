d507c942fa073911833e7597ea5d69376ccd684a
NITRO <sanadylov@gmail.com>
2023-09-19 20:48:17 +0600

    Add getAuthentication

--- src/main/java/com/example/lms/configs/security/JWTService.java
+++ src/main/java/com/example/lms/configs/security/JWTService.java
@@ -4,19 +4,26 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.lms.entities.User;
+import com.example.lms.exeptions.NotFoundException;
+import com.example.lms.repository.UserRepository;
 import java.time.ZonedDateTime;
 import java.util.Date;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class JWTService {
 
   @Value("${secret_key}")
   private static String SECRET_KEY;
+  private final UserRepository userRepository;
 
   public String generateToken(UserDetails userDetails) {
     return JWT.create()
@@ -34,5 +41,11 @@ public class JWTService {
     return jwt.getClaim("username").asString();
   }
 
-
+  public User getAuthentication() {
+    String username = SecurityContextHolder.getContext().getAuthentication().getName();
+    return userRepository.getUserByEmail(username).orElseThrow(() -> {
+      log.error("User with email: %s not found".formatted(username));
+      return new NotFoundException("User with email: %s not found".formatted(username));
+    });
+  }
 }
