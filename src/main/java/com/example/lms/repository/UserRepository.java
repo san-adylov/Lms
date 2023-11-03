7490956015ac5bfcc8fb236e354f12e95fe7de6a
Sanzhar <sanadylov@gmail.com>
2023-11-03 16:51:00 +0600

    Fixed UserRepository.java and add dependencies

--- src/main/java/com/example/lms/repository/UserRepository.java
+++ src/main/java/com/example/lms/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.lms.repository;
 
 import com.example.lms.entities.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
