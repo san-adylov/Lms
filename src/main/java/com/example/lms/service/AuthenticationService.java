b7078fa396aed0535898506e3f9abb70efad7871
Sanzhar <sanadylov@gmail.com>
2023-11-08 00:39:49 +0600

    Add method

--- src/main/java/com/example/lms/service/AuthenticationService.java
+++ src/main/java/com/example/lms/service/AuthenticationService.java
@@ -2,9 +2,15 @@ package com.example.lms.service;
 
 import com.example.lms.dto.request.authentication.SignInRequest;
 import com.example.lms.dto.response.authentication.AuthenticationResponse;
+import com.example.lms.dto.response.simple.SimpleResponse;
+import jakarta.mail.MessagingException;
 
 public interface AuthenticationService {
 
     AuthenticationResponse signIn(SignInRequest signInRequest);
+
+    SimpleResponse sendPasswordToEmail(String email, String link) throws MessagingException;
+
+
 
 }
