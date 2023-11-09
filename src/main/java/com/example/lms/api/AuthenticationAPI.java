7285812a8768a8d18b604e82fee112b0b9ca1fbd
Sanzhar <sanadylov@gmail.com>
2023-11-10 00:53:40 +0600

    add api

--- src/main/java/com/example/lms/api/AuthenticationAPI.java
+++ src/main/java/com/example/lms/api/AuthenticationAPI.java
@@ -1,15 +1,15 @@
 package com.example.lms.api;
 
+import com.example.lms.dto.request.authentication.RecoveryPasswordRequest;
 import com.example.lms.dto.request.authentication.SignInRequest;
 import com.example.lms.dto.response.authentication.AuthenticationResponse;
+import com.example.lms.dto.response.simple.SimpleResponse;
 import com.example.lms.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +23,18 @@ public class AuthenticationAPI {
     @Operation(summary = "Sign in", description = "Sign in with email")
     public AuthenticationResponse signIn(@RequestBody SignInRequest signInRequest) {
         return authenticationService.signIn(signInRequest);
+    }
+
+    @PostMapping("/sendEmail")
+    @Operation(summary = "Send email", description = "Send link to email")
+    public SimpleResponse sendLinkToEmail(@RequestParam String emailAddress, @RequestParam String link) throws MessagingException {
+        return authenticationService.sendPasswordToEmail(emailAddress, link);
+    }
+
+    @PostMapping("/recover-password/{userId}")
+    @Operation(summary = "Recovery password", description = "Recovery password by student through email")
+    public SimpleResponse recoveryPassword( @RequestBody RecoveryPasswordRequest recoveryPasswordRequest) {
+        return authenticationService.recoveryPassword(recoveryPasswordRequest);
     }
 
 
