044bbda53b327bbd58086a06ec21b420614e05b9
Sanzhar <sanadylov@gmail.com>
2023-11-14 16:03:35 +0600

    add3

--- src/main/java/com/example/lms/repository/LinkRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lms.repository;
-
-import com.example.lms.entities.Link;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LinkRepository extends JpaRepository<Link,Long> {
-}
