bb77e2db4bf83951ed8d0baeecd50dbf727317da
Sanzhar <sanadylov@gmail.com>
2023-11-14 16:04:35 +0600

    add7

--- src/main/java/com/example/lms/repository/TestAnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lms.repository;
-
-import com.example.lms.entities.TestAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestAnswerRepository extends JpaRepository<TestAnswer,Long> {
-}
--- src/main/java/com/example/lms/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lms.repository;
-
-import com.example.lms.entities.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test,Long> {
-}
