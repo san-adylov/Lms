1cbcf4c3f2a3471bbb46de578fe6a895468478a6
Sanzhar <sanadylov@gmail.com>
2023-11-14 16:04:42 +0600

    add8

--- src/main/java/com/example/lms/repository/VideoLessonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lms.repository;
-
-import com.example.lms.entities.VideoLesson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoLessonRepository extends JpaRepository<VideoLesson,Long> {
-}
